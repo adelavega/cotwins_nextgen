bf0ea6eb79c91f8e1cd8dd8d1f73771abd3cb214
Alejandro de la Vega <delavega@colorado.edu>
2016-03-18 21:50:22 -0600

    updated config

--- wsgi/config.py
+++ wsgi/config.py
@@ -1,16 +1,15 @@
-import os
+qimport os
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = '\xc8U;\xe2w\xb3[c-:\xceeKu\xc9f\xd2\xaac\xfb\x1dZ\xc2\xc2'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    DASHBOARD_PASS = 'cortex24!'
-
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 
 
 class StagingConfig(Config):
