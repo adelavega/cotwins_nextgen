7c8529d20dedb05032d7cae19681dfcc88952cf7
Alejandro <delavega@colorado.edu>
2016-09-28 12:33:16 -0500

    restored config

---
+++ wsgi/config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    DEVELOPMENT = False
+    TESTING = False
+    CSRF_ENABLED = True
+    EXP_DEBUG = False
+
+    try:
+        SQLALCHEMY_DATABASE_URI = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+    except:
+        SQLALCHEMY_DATABASE_URI = 'postgres://localhost/assesment'
+
+class ProductionConfig(Config):
+    pass
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    EXP_DEBUG = True
+
+
+
+
